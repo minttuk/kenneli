cb52305a3ce4352fcc3050f393ceda805cb04d90
Minttu Koponen <minttu.koponen@metropolia.fi>
2016-11-30 10:47:33 +0000

    NYT TOIMII! messagen siirto mysql:sta koira1:lle

--- SQLscripts.sql
+++ SQLscripts.sql
@@ -46,3 +46,4 @@ insert into dog (name, owner, description) values ("musti", "1", "Kiltti koira."
 
 insert into message (title, message, user_id, dog_id) values ("Hei Koiraihmiset!", "Minulla on ilo kertoa tästä sivusta teille.", "1", "1");
 insert into message (title, message, user_id, dog_id) values ("Musti karkasi", "Onko kukaan nähnyt mustia!!??!?", "1", "1");
+insert into message (title, message, user_id, dog_id) values ("Musti", "MessageMusti", "1", "1");
